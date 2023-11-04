270a8f26a5d86761bed767b2a7532513952f6af8
anndra17 <andra.horhocea@student.unitbv.ro>
2023-11-04 16:09:06 +0200

    Am creat metoda pentru validarea pilonului.

--- Twixt/Twixt/peg.h
+++ Twixt/Twixt/peg.h
@@ -1,9 +1,9 @@
 #pragma once
-
 #include<cstdint>
-
 #include<iostream>
 #include<vector>
+
+#include "board.h"
 
 enum class Color :uint8_t {
 	Red,
@@ -16,10 +16,28 @@ class Peg {
 		Peg(const Peg& other);
 		Peg& operator=(const Peg& obj);
 		~Peg() = default;
+
 		void SetColor(Color color);
-		void SetPosition(std::pair<int, int> pozitie);
+		void SetPosition(std::pair<int, int> position);
 		const Color& GetColor() const;
 		const std::pair<int, int>& GetPosition() const;
+
+		bool pegValidation(int& row, int& col, int& boardSize, Board& boardGame) {
+			std::pair<int, int> position;
+			position.first = row;
+			position.second = col;
+
+			if (position.first <= 0 || position.first >= boardSize || position.second <= 0 || position.second >= boardSize) {
+				return false;
+			}
+			
+			if (boardGame[position].has_value()) {
+				return false;
+			}
+
+			return true;
+		}
+
 	private:
 		Color m_color : 1;
 		std::pair<int, int>m_position;
