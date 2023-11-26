2c08d80ee2827e864ad283d8ae5ae5f1840c4a0f
anndra17 <andra.horhocea@student.unitbv.ro>
2023-11-26 22:20:26 +0200

    Rule of 0 pentru clasa Peg.

--- Twixt/Twixt/peg.cpp
+++ Twixt/Twixt/peg.cpp
@@ -1,73 +1,73 @@
 #include "peg.h"
 
 Peg::Peg(Color color, Position position)
-	:m_color{ color }, m_position{ position }
+    :m_color{ color }, m_position{ position }
 {
 }
 Peg::Peg(const Peg& other)
-	:m_color{ other.m_color }, m_position{ other.m_position }, m_adjacencyPegs{ other.m_adjacencyPegs }
+    :m_color{ other.m_color }, m_position{ other.m_position }, m_adjacencyPegs{ other.m_adjacencyPegs }
 {
 }
 Peg::Peg(Peg&& peg) noexcept //?
 {
-	swap(peg);
+    swap(peg);
 }
 Peg& Peg::operator=(const Peg& obj)
 {
-	Peg aux{ obj };
-	swap(aux);
-	return *this;
+    Peg aux{ obj };
+    swap(aux);
+    return *this;
+}
+Peg& Peg::operator=(Peg&& peg) noexcept
+{
+    swap(peg);
+    return *this;
 }
 bool Peg::operator==(const Peg& obj)
 {
-	return m_color == obj.m_color && m_position == obj.m_position;
+    return m_color == obj.m_color && m_position == obj.m_position;
 }
 void Peg::SetColor(Color color)
 {
-	m_color = color;
+    m_color = color;
 }
 
 void Peg::SetPosition(Position position)
 {
-	m_position = position;
+    m_position = position;
 }
 
 const Color& Peg::GetColor() const
 {
-	return m_color;
+    return m_color;
 }
 
 const Peg::Position& Peg::GetPosition() const
 {
-	return m_position;
+    return m_position;
 }
 
 const std::vector<Peg>& Peg::GetAdjacencyPegs() const
 {
-	return m_adjacencyPegs;
+    return m_adjacencyPegs;
 }
 
 void Peg::swap(Peg& peg)
 {
-	Color color{ peg.m_color };
-	peg.m_color = m_color;
-	m_color = color;
+    Color color{ peg.m_color };
+    peg.m_color = m_color;
+    m_color = color;
 
-	Position position{ peg.m_position };
-	peg.m_position = m_position;
-	m_position = position;
-
-	std::vector<Peg> adyacency{ peg.m_adjacencyPegs };
-	peg.m_adjacencyPegs = m_adjacencyPegs;
-	m_adjacencyPegs = adyacency;
+    std::swap(m_position, peg.m_position);
+    std::swap(m_adjacencyPegs, peg.m_adjacencyPegs);
 }
 
 void Peg::addAdjacentPeg(const Peg& peg)
 {
-	m_adjacencyPegs.push_back(peg);
+    m_adjacencyPegs.push_back(peg);
 }
 
 std::ostream& operator<<(std::ostream& os, const Peg& peg)
 {
-	return os << static_cast<int>(peg.GetColor());
+    return os << static_cast<int>(peg.GetColor());
 }
--- Twixt/Twixt/peg.h
+++ Twixt/Twixt/peg.h
@@ -5,35 +5,35 @@
 
 
 enum class Color :uint8_t {
-	Red,
-	Black
+    Red,
+    Black
 };
 class Peg {
 public:
-	using Position = std::pair<size_t,size_t>;
-	Peg(Color color, Position position);
-	Peg() = default;
-	Peg(const Peg& other);
-	Peg(Peg&& peg) noexcept;
-	Peg& operator=(const Peg& obj);
-	bool operator==(const Peg& obj);
+    using Position = std::pair<size_t, size_t>;
+    Peg(Color color, Position position);
+    Peg() = default;
+    Peg(const Peg& other);
+    Peg(Peg&& peg) noexcept;
+    Peg& operator=(const Peg& obj);
+    Peg& operator=(Peg&& peg) noexcept;
+    bool operator==(const Peg& obj);
 
-	~Peg() = default;
+    ~Peg() = default;
 
-	void SetColor(Color color);
-	void SetPosition(Position position);
-	const Color& GetColor() const;
-	const Position& GetPosition() const;
-	const std::vector<Peg>& GetAdjacencyPegs() const;
+    void SetColor(Color color);
+    void SetPosition(Position position);
+    const Color& GetColor() const;
+    const Position& GetPosition() const;
+    const std::vector<Peg>& GetAdjacencyPegs() const;
 
-	void swap(Peg& peg);
-	void addAdjacentPeg(const Peg& peg);
+    void swap(Peg& peg);
+    void addAdjacentPeg(const Peg& peg);
 
 private:
-	Color m_color : 1;
-	Position m_position;
-	std::vector<Peg>m_adjacencyPegs;
+    Color m_color : 1;
+    Position m_position;
+    std::vector<Peg>m_adjacencyPegs;
 
 };
-
 std::ostream& operator<<(std::ostream& os, const Peg& peg);
