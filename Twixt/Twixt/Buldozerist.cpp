c1e28cf00e50d7c92ff51f0f9f6beb2950fd7262
anndra17 <andra.horhocea@student.unitbv.ro>
2023-12-10 20:39:35 +0200

    Implementare constructor de copiere și operator de atribuire de copiere sigure pentru clasa Buldozerist

--- Twixt/Twixt/Buldozerist.cpp
+++ Twixt/Twixt/Buldozerist.cpp
@@ -3,3 +3,20 @@
 Buldozerist::Buldozerist(Board& gameBoard) : board{ &gameBoard }
 {
 }
+
+Buldozerist::Buldozerist(const Buldozerist& other)
+	: board(other.board), currentPosition(other.currentPosition) {}
+
+Buldozerist& Buldozerist::operator=(const Buldozerist& other)
+{
+	if (this != &other)
+	{
+		delete board;
+
+		board = other.board;
+		currentPosition = other.currentPosition;
+	}
+
+	return *this;
+}
+
--- Twixt/Twixt/Buldozerist.h
+++ Twixt/Twixt/Buldozerist.h
@@ -7,6 +7,9 @@ public:
 	Buldozerist(Board& gameBoard);
 	Buldozerist() = default;
 	~Buldozerist() = default;
+	Buldozerist(const Buldozerist& other);
+	Buldozerist& operator=(const Buldozerist& other);
+
 private:
 	Board* board;
 	Board::Position currentPosition;
