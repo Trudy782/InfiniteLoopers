eebcae9c84a89047ff1df8889c13e23948c1ada1
anndra17 <andra.horhocea@student.unitbv.ro>
2024-01-14 19:24:06 +0200

    Am facut constructorul clasei CustomLine si am definit functia RemoveLineByStartEnd

--- Twixt/Twixt/CustomLine.cpp
+++ Twixt/Twixt/CustomLine.cpp
@@ -1,1 +1,19 @@
 #include "CustomLine.h"
+
+CustomLine::CustomLine(const QPointF& p1, const QPointF& p2, bool active, Position Start, Position End)
+    : QLineF{ p1, p2 }, isActive{ active }, start{ Start }, end{ End } {}
+
+void RemoveLineByStartEnd(QList<CustomLine>& lines, const Position& start, const Position& end)
+{
+    auto it = std::remove_if(lines.begin(), lines.end(), [&](const CustomLine& line) {
+        return line.start == start && line.end == end;
+        });
+
+    if (it != lines.end()) {
+        lines.erase(it, lines.end());
+        qDebug() << "Line removed!";
+    }
+    else {
+        qDebug() << "Line not found!";
+    }
+}
